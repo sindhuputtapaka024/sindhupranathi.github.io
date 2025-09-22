1abe7bcddfc163275860f0cc761eacdeb1d43204
sindhuputtapaka024 <sindhupranathi02@gmail.com>
2025-09-22 13:46:34 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+ACCESS_TOKEN = st.secrets["goto"]["GOTO_ACCESS_TOKEN"]
+ACCOUNT_KEY = st.secrets["goto"]["GOTO_ACCOUNT_KEY"]
+BASE_URL = st.secrets["goto"]["GOTO_BASE_URL"]
